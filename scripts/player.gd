10026b1944a57a46cd22b3ea37a9cab18ddec9a1
iPwaoo <quentin.bouetel@gmail.com>
2024-09-19 17:47:02 -0400

    debug de l'animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,8 +12,6 @@ func _physics_process(delta: float) -> void:
 	var direction_x_axis := Input.get_axis("left_axis", "right_axis")
 	var direction_y_axis := Input.get_axis("up_axis", "down_axis")
 	
-
-	
 	if direction_x_axis or direction_y_axis:
 		velocity.x = direction_x_axis * SPEED
 		velocity.y = direction_y_axis * SPEED
@@ -23,7 +21,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-	if animated_sprite_2d.animation != ANIMATION_IDLE:
-		animated_sprite_2d.animation = ANIMATION_IDLE
+		if animated_sprite_2d.animation != ANIMATION_IDLE:
+			animated_sprite_2d.animation = ANIMATION_IDLE
 
 	move_and_slide()
