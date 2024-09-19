b9ca9d639699c4b085b0e4a07e05413a6233efcb
iPwaoo <quentin.bouetel@gmail.com>
2024-09-18 23:38:54 -0400

    changement fonctionnement de deplacement + ajout d'une animation pendant le déplacement + limite de la camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,29 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+const ANIMATION_IDLE = "idle"
+const ANIMATION_RUN = "run"
 const SPEED = 150
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-
 	var direction_x_axis := Input.get_axis("ui_left", "ui_right")
-	if direction_x_axis:
-		velocity.x = direction_x_axis * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-		
-	var direction_y_axis := -Input.get_axis("ui_down","ui_up")
-	if direction_y_axis:
-		velocity.y = direction_y_axis * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+	var direction_y_axis := -Input.get_axis("ui_down", "ui_up")
 
+	
+	# Vérifie si le joueur se déplace
+	if direction_x_axis or direction_y_axis:
+		velocity.x = direction_x_axis * SPEED
+		velocity.y = direction_y_axis * SPEED
+		# Si le joueur bouge, on lance l'animation "run"
+		if animated_sprite_2d.animation != ANIMATION_RUN:
+			animated_sprite_2d.animation = ANIMATION_RUN
+	else:
+		velocity.x = 0 #move_toward(velocity.x, 0, SPEED)
+		velocity.y = 0 #move_toward(velocity.y, 0, SPEED)
+		# Si aucune direction n'est active, on joue l'animation "idle"
+		if animated_sprite_2d.animation != ANIMATION_IDLE:
+			animated_sprite_2d.animation = ANIMATION_IDLE
 	move_and_slide()
