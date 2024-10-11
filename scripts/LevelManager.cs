1b2b10c089354b02c554c166ed4842ab1a7bfc7b
huseyinC1876 <117670955+huseyinC1876@users.noreply.github.com>
2024-10-11 04:32:12 +0100

    Update LevelManager.cs

--- scripts/LevelManager.cs
+++ scripts/LevelManager.cs
@@ -2,7 +2,20 @@ using Godot;
 
 public partial class LevelManager : Godot.Control
 {
-	//CustomMainLoopCS mainLoop =  null;
+	CustomMainLoopCS mainLoop =  CustomMainLoopCS.GetInstance();
+	
+	private static LevelManager _instance;
+	
+	public static LevelManager GetInstance()
+	{
+		return _instance;
+	}
+	
+	public void _Initialize()
+	{
+		// Initialiser le singleton
+		_instance = this;
+	}
 	
 	
 	public void StartButtonPressed()
@@ -12,8 +25,18 @@ public partial class LevelManager : Godot.Control
 	
 	public void LoadButtonPressed()
 	{
-		//SaveManager sm = mainLoop.getSaveManager();
-		//sm.LoadGame();
+		if (mainLoop == null){
+			GD.Print("null Main Loop");
+		}
+		else {
+			SaveManager sm = mainLoop.GetSaveManager();
+			if ( sm != null){
+				sm.LoadGame();
+			}
+			else {
+				GD.Print("Save And Quit");
+			}
+		}
 	}
 	
 	public void _on_options_button_pressed()
